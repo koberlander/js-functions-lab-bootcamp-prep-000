213c178f45ddf2ea7a5b93943b3c5e0f30746a83
ko kristin <koberlander115@gmail.com>
2019-01-14 20:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // write your code below!
 
 var name = 'you';
-var holiday = 'Independence Day";
-var days = 20;
+var holiday = 'Independence Day';
+var days = '20';
 
 function happyHolidays() {
   return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!";
+  return 'Happy holidays, ${name}!';
 }
 
 function happyHolidayTo(holiday, name){
