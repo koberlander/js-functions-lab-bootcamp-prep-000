b697e02f04252d401c646bdedbbfa15262cf11e9
ko kristin <koberlander115@gmail.com>
2019-01-14 19:54:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // write your code below!
 
-var holiday = "independence Day";
-
+var name = "you";
 
 function happyHolidays() {
   return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  var name = "you";
   return "Happy holidays, ${name}!";
 }
 
 function happyHolidayTo(holiday, name){
+  holiday = "Independence Day";
   return "Happy ${holiday}, ${name}!";
 }
 
 function holidayCountdown(holiday, days){
+ days = 20;
+ holiday = "Mother's Day"; 
   return "It's ${days} days until ${holiday}!";
 }
